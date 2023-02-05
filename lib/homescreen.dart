6733f9703365ba996dae048aa01c8627882f5eea
Rajesh Kumar <81349233+rajbhattia@users.noreply.github.com>
2023-02-05 00:41:25 -0800

    gridview added

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -13,18 +13,41 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         title: Text('list view and Grid view'),
       ),
-      body: ListView.builder(
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 20,crossAxisSpacing: 20),
         itemCount: fruitnames.length,
         itemBuilder: (context,index){
-        return Card(
-           color: Colors.blue,
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(fruitnames[index]),
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+           alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(4)),
+            //  
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Center(child: Text(fruitnames[index])),
+              ),
             ),
-          );
+        );
       
-      })
+      }
+      )
+
+      // body: ListView.builder(
+      //   itemCount: fruitnames.length,
+      //   itemBuilder: (context,index){
+      //   return Card(
+      //      color: Colors.blue,
+      //       child: Padding(
+      //         padding: const EdgeInsets.all(15.0),
+      //         child: Text(fruitnames[index]),
+      //       ),
+      //     );
+      
+      // }
+      // )
     );
   }
 }
